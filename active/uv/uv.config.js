5be020e4fbece58738401f81e854d0c7176d65a7
Orbit. <159073116+1056516@users.noreply.github.com>
2025-03-19 22:37:39 -0400

    Update uv.config.js

--- active/uv/uv.config.js
+++ active/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/active/go/',
-    bare:'https://mathiscoolpitheratioofacirclecircumfrencetoitsdiameter.3.141.meteomanolo.es/bare/',
+    bare:'https://mathinformativebrutusiscool.potomacriversafetycommittee.org/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/active/uv/uv.handler.js',
